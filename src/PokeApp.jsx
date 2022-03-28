75ee61919064a4f5e750713a3fc9ca8abf0705ff
Benja253 <benja48.9@gmail.com>
2022-03-28 01:09:17 -0300

    add folder assets home

--- src/PokeApp.jsx
+++ src/PokeApp.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 
 const PokeApp = () => {
   return (
-    <div>Hola mundo</div>
+    <div>
+      <HashRouter>
+        <Routes>
+          <Route path='/' element={<h1>Hola mundo</h1>}></Route>
+          <Route path='/pokemon' element={<h2>PokeApp</h2>}></Route>
+        </Routes>
+      </HashRouter>
+    </div>
   )
 }
 

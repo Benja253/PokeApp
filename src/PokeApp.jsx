96f5f627830f643b42f12e68fa833f44b8c07ba0
Benja253 <benja48.9@gmail.com>
2022-03-28 00:36:52 -0300

    First commit

---
+++ src/PokeApp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PokeApp = () => {
+  return (
+    <div>Hola mundo</div>
+  )
+}
+
+export default PokeApp
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import PokeApp from './PokeApp'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <PokeApp />
+  </React.StrictMode>,
+  document.getElementById('root')
+)

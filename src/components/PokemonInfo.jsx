4a67600eab44a707e628bbe7c904572c326074c9
Benja253 <benja48.9@gmail.com>
2022-04-06 16:57:12 -0400

    fix error in import

--- src/components/PokemonInfo.jsx
+++ src/components/PokemonInfo.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-import CardInfo from './cardInfo'
+import CardInfo from './CardInfo'
 import HeaderPokemon from './HeaderPokemon'
 import Movements from './Movements'
 import axios from 'axios'
